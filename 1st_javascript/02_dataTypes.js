b94c8d048c493a10287e90a7594d262016fd506e
Sudhanshu Gupta <160475122+Sudhanshu-code@users.noreply.github.com>
2024-02-20 09:00:49 +0000

    datatypes

---
+++ 1st_javascript/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat js code as newer version
+
+// alert(3+3) //we are using Nodejs, not browser
+
+let name = "Sudhanshu"   //String
+let age =15;               //number
+let isvalid = true      //boolean
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean =>true/false
+// null => standalone value
+// undefined => value not assigned => const age;
+//symbol => unique
+
+// object
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
